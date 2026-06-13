b099d7b710c665faa9e0aea706f20af18b230f22
monikanimmana <monikanimmana3366@gmail.com>
2026-06-13 13:16:15 +0530

    Assigned offer complaints

--- complaints/urls.py
+++ complaints/urls.py
@@ -7,5 +7,7 @@ urlpatterns =[
     path('updatecomplaint/<int:pk>/',UpdateComplaint.as_view(),name='update_complaint'),
     path('details/<int:pk>',ComplaintDetails.as_view(),name='complaint_deatils'),
     path('delete/',DeleteComplaint.as_view(),name='delete_complaint'),
-    path('')
+    path('assign-officer/<int:pk>',AssignOfficer.as_view(),name="assign_officer"),
+    path('officer-complaints/',ListOfficerComplaints.as_view(),name="officer_complaints"),
+    path(),
 ]
