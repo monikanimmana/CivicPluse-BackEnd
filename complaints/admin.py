aff36ce29553df5395064ebdf77e1abb481db285
monikanimmana <monikanimmana3366@gmail.com>
2026-06-15 10:20:55 +0530

    seaprated feilds

--- complaints/admin.py
+++ complaints/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Complaint
+from users.models import *
 
 # Register your models here.
-admin.site.register(Complaint)
+def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    obj_id = request.resolver_match.kwargs.get("object_id")
+
+    if obj_id:
+        complaint = Complaint.objects.get(pk=obj_id)
+
+        if db_field.name == "assignedOfficer":
+            kwargs["queryset"] = User.objects.filter(
+                role="officer",
+                department=complaint.department
+            )
+
+        elif db_field.name == "assignedWorker":
+            kwargs["queryset"] = User.objects.filter(
+                role="worker",
+                department=complaint.department
+            )
+
+    return super().formfield_for_foreignkey(
+        db_field, request, **kwargs
+    )
