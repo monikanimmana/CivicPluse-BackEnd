962aa37cb8ea55a10a042194ac69d8cc05ebc5c1
monikanimmana <monikanimmana3366@gmail.com>
2026-06-15 09:44:38 +0530

    department medoe

--- departments/models.py
+++ departments/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-class Department():
+class Department(models.Model):
+   dept_id=models.CharField(max_length=10)
+   dept_name=models.CharField(max_length=20)
+   dept_description=models.TextField()
+   is_active=models.BooleanField(default=True)
+
+   def __str__(self):
+      return f"{self.dept_name} ({self.is_active})"
     
